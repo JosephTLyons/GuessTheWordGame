3f4fe1ff258547a6be3332f4a5638e069df46797
Joseph Lyons <joe_04_04@yahoo.com>
2016-07-29 03:34:47 -0400

    Initial Commit

---
+++ Guess the Word Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Guess the Word Game
+//
+//  Created by Joseph Lyons on 7/29/16.
+//  Copyright © 2016 Joseph Lyons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
