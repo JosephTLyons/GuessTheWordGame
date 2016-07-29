4c04a22b8714b8ac428f5206209405ba3d41bff8
Joseph Lyons <joe_04_04@yahoo.com>
2016-07-29 07:17:06 -0400

    Implemented Letter Pool

--- Guess the Word Game/main.cpp
+++ Guess the Word Game/main.cpp
@@ -9,8 +9,17 @@ void ClearScreen();
 void InitializeUnderlinesAndBlankSpaces(const vector <char> &WordToGuess, vector <char> &CorrectLetters);
 void GuessWord(const vector <char> &WordToGuess, vector <char> &CorrectLetters);
 bool SeeIfLetterGuessedIsInWord(const char &Input, const vector <char> &WordToGuess, int &PositionOfFoundLetter);
+void RemoveGuessedLetterFromLetterPool(char LetterPool[], const char &Input);
 void InsertCorrectLetterInCorrectLetterVector(const vector <char> &WordToGuess, const int &PositionOfFoundLetter, vector <char> &CorrectLetters);
 void TemporaryIndicationOfIncorrectGuess(const int NumOfIncorrectGuesses);//to be rewritten to display hangman
+
+/* 
+ 
+ HOW TO DEAL WITH WORDS WITH MULTIPLE SAME LETTERS?
+ DISPLAY FULL ALPHABET AND REMOVE THEM AS THE ARE USE
+ DISPLAY ALL LETTERS USED
+ 
+*/
 
 int main()
 {
@@ -72,10 +81,15 @@ void GuessWord(const vector <char> &WordToGuess, vector <char> &CorrectLetters)
 {
     char Input;
     int NumberOfIncorrectGuesses = 0;//hard coded for now
-    int PositionOfFoundLetter = -1;  //initialized at -1 because it uses array style notation, which starts at 0
+    int PositionOfFoundLetter;
+    
+    /* PLACED SPACE BEFORE A SO I COULD USE 1 INSTEAD OF 0 IN ARRAY NOTATION TO ACCESS THE FIRST LETTER */
+    char LetterPool[] = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
     
     do
     {
+        PositionOfFoundLetter = -1;//SET AT -1 BECAUSE ITS USED WITH ARRAY STYLE NOTATION, WHICH STARTS A 0
+        
         /* DISPLAY CORRECT LETTERS */
         
         cout << "Word to Guess: ";
@@ -85,10 +99,27 @@ void GuessWord(const vector <char> &WordToGuess, vector <char> &CorrectLetters)
             cout << CorrectLetters[i];
         }
         
+        /* DISPLAY LETTER POOL TO GUESS FROM */
+        
+        cout << "\n\nLetter pool: \n\n";
+        
+        for (int i = 1; LetterPool[i] != 0; i++)
+        {
+            cout << LetterPool[i];
+            
+            /* ADD NEWLINE EVERY 4 LETTERS */
+            
+            if (i % 8 == 0)
+                cout << '\n';
+        }
+        
         cout << "\n\nGuess one letter: ";
         
         cin >> Input;
+        Input = toupper(Input);// CONVERT TO UPPPERCASE SO THERE'S NO CASE ISSUES
         cin.ignore();// IGNORE NEWLINE AFTER CIN >>
+        
+        RemoveGuessedLetterFromLetterPool(LetterPool, Input);
         
         /* SEEIFLETTERGUESSEDISINWORD WILL RETURN A TRUE IF LETTER IS FOUND, ONLY INCREMENT COUNTER IF LETTER ISN'T FOUND */
         /* IT ALSO HAS A REFERENCE PARAMETER THAT RETURNS THE POSITION OF A LETTER FOUND, IF IT IS FOUND */
@@ -116,13 +147,24 @@ void GuessWord(const vector <char> &WordToGuess, vector <char> &CorrectLetters)
     
 }
 
+void RemoveGuessedLetterFromLetterPool(char LetterPool[], const char &Input)
+{
+    for (int i = 0; LetterPool[i] != 0; i++)
+    {
+        if (LetterPool[i] == Input)
+        {
+            LetterPool[i] = '_';
+        }
+    }
+}
+
 bool SeeIfLetterGuessedIsInWord(const char &Input, const vector <char> &WordToGuess, int &PositionOfFoundLetter)
 {
     for (int i = 0; i < WordToGuess.size(); i++ )
     {
         /* CHECKING TO SEE IF LETTER IS ANYWHERE IN WORDTOGUESS, BY ITERATING THROUGH IT */
         
-        if (toupper(Input) == WordToGuess[i])
+        if (Input== WordToGuess[i])
         {
             PositionOfFoundLetter = i;
             return true;
