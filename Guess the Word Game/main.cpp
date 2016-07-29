83b3c5e5b0954cfa4a8306e3d880dc26281c8b26
Joseph Lyons <joe_04_04@yahoo.com>
2016-07-29 03:55:44 -0400

    First Commit

--- Guess the Word Game/main.cpp
+++ Guess the Word Game/main.cpp
@@ -1,15 +1,58 @@
-//
-//  main.cpp
-//  Guess the Word Game
-//
-//  Created by Joseph Lyons on 7/29/16.
-//  Copyright © 2016 Joseph Lyons. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+void EnterWord(vector <char> &WordToBeGuessed);
+void ClearScreen();
+void TemporaryIndicationOfIncorrectGuess(int NumOfIncorrectGuesses);
+
+int main()
+{
+    int NumberOfIncorrectGuesses = 6;
+    vector <char> WordToBeGuessed;
+    
+    EnterWord(WordToBeGuessed);
+    
+    /* ADD NEWLINES */
+    
+    ClearScreen();
+    
+    TemporaryIndicationOfIncorrectGuess(NumberOfIncorrectGuesses);
+}
+
+void EnterWord(vector <char> &WordToBeGuessed)
+{
+    char Input;
+    
+    cout << "Enter word to be guessed: ";
+    
+    cin.get(Input);
+    
+    while (Input != '\n')
+    {
+        WordToBeGuessed.push_back(Input);
+        cin.get(Input);
+    }
+}
+
+void ClearScreen()
+{
+    /* ADD 30 NEWLINES TO CLEAR SCREEN SO NEXT PLAYER CAN'T SEE PREVIOUSLY ENTERED WORD */
+    /* NUMBER CAN BE CHANGED AT ANY TIME, SIMPLY ADJUST i CONDITION */
+    
+    for (int i = 0; i < 30; i++)
+    {
+        cout << "\n";
+    }
+}
+
+void TemporaryIndicationOfIncorrectGuess(int NumOfIncorrectGuesses)
+{
+    cout << "Incorrect guesses: ";
+    
+    for (int i = 0; i <NumOfIncorrectGuesses; i++)
+    {
+        cout << "X ";
+    }
 }
