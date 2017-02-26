272a41080f97e4c0a31a3bce5ce002172718fd10
Joseph Lyons <josephlyons@josephs-air.pk5001z>
2017-02-26 04:08:53 -0500

    Incremental commit

--- Guess the Word Game/main.cpp
+++ Guess the Word Game/main.cpp
@@ -6,6 +6,7 @@ using namespace std;
     FIX SPACING IN OUTPUT
     CHANGE Xs TO THE HANGMAN DRAWING
     CHANGE DOUBLE SPACING TO SINGLE SPACING?
+    Guessing the same wrong letter twice shouldn't count as an incorrect guess
 */
 
 int main()
